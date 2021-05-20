7694ff83351ec39b893d0d4464376a0b5aa927e6
jabc300 <jabc300@gmail.com>
2021-05-20 13:50:52 -0700

    Add Project

---
+++ Assets/Scripts/CarAgent.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+public class CarAgent : Agent
+{
+    public float SteerForce, MotorForce;
+    public Transform initialPosition;
+    public WheelCollider FR_L_Wheel, FR_R_Wheel, RE_L_Wheel, RE_R_Wheel;
+    public override void OnEpisodeBegin()
+    {
+        transform.position = initialPosition.position;
+        transform.eulerAngles = new Vector3(0, 90, 0);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        int selection = actions.DiscreteActions[0];
+        float h;
+        float v = -MotorForce;
+
+        /*Actions in selection
+         0: Don't steer, go straight
+         1: Steer to the right
+         2: Steer to the left
+         */
+
+        Debug.Log(actions.DiscreteActions[0]);
+
+        if(selection <= 1)
+        {
+            h = selection;
+        }else
+        {
+            h = -1;
+        }
+
+        RE_R_Wheel.motorTorque = v;
+        RE_L_Wheel.motorTorque = v;
+
+        FR_L_Wheel.steerAngle = h * SteerForce;
+        FR_R_Wheel.steerAngle = h * SteerForce;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Target"))
+        {
+            SetReward(+1f);
+        }
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            SetReward(-5f);
+            EndEpisode();
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            SetReward(+5f);
+            EndEpisode();
+        }
+    }
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public float MotorForce;
+    public WheelCollider RE_L_Wheel, RE_R_Wheel;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float v = -MotorForce;
+
+        RE_R_Wheel.motorTorque = v;
+        RE_L_Wheel.motorTorque = v;
+
+
+        /*if(Input.GetAxis("Vertical") == 0)
+        {
+            RE_L_Wheel.brakeTorque = BrakeForce;
+            RE_R_Wheel.brakeTorque = BrakeForce;
+        }
+        else
+        {
+            RE_L_Wheel.brakeTorque = 0;
+            RE_R_Wheel.brakeTorque = 0;
+        }*/
+    }
+}
